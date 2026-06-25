b2c75988cd7d26608e797afee117754e3f7d09ca
llcfernandes <lucasmividi@gmail.com>
2026-06-25 11:36:07 -0300

    corrige importação do PrismaClient no arquivo server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { PrismaClient } from "./generated/prisma/index.js"
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
